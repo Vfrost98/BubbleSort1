d184610eb51971ae6cc9606028e52b4c85b5fa35
VeeisMe <41344497+Vfrost98@users.noreply.github.com>
2021-05-03 09:52:08 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int A[], int x){
+  int temp;
+
+  for(int i = 0; i < x-1; i++){
+    for(int j = 0; j < x-i-1; j++ ){
+      if(A[j] > A[j+1]){
+        temp = A[j];
+        A[j] = A[j+1];
+        A[j+1] = temp; 
+      }
+    }
+  }
+}
+
+int main() {
+  int A[5] = {3, 2, 4, 8, 9}; //created array
+  int aSized = 5-1; //array size
+
+  cout << "Unsorted array: " << endl;
+  for(int i = 0; i < 5; i++){
+      cout << A[i] << " ";
+      
+  }
+  
+  cout << endl;
+
+  bubbleSort(A,aSized);
+
+  cout << endl;
+
+  cout << "Sorted array: " << endl;
+  for(int i = 0; i < 5; i++){
+      cout << A[i] << " ";
+      
+  }
+
+  cout << endl;
+}
